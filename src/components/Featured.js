b8205c5ddaeeb90e1222ed93386b7cfd6e1a1466
Siljesandersen <silje_sandersen@hotmail.com>
2022-01-12 14:01:33 +0100

    feat: created homepage

---
+++ src/components/Featured.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Featured() {
+  return (
+    <div className="highlights">
+      <h3>Project highlights</h3>
+      <div className="highlights__cont">
+        <div className="highlights__cont--item"></div>
+        <div className="highlights__cont--item"></div>
+        <div className="highlights__cont--item"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Featured;
---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+import profile from "../profile.jpg";
+
+function Hero() {
+  return (
+    <div className="introduction">
+      <div className="introduction__text">
+        <h1>Hello World!</h1>
+        <h2>I am Silje the Front-End Developer</h2>
+      </div>
+      <div className="introduction__img">
+        <img src={profile} alt="profile image" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
