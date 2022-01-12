ec9344f7bf4e179cbc5b1d93082fb667bb6b28f6
Siljesandersen <silje_sandersen@hotmail.com>
2022-01-12 14:31:28 +0100

    fix: added images to project highlights

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,13 +1,22 @@
 import React from "react";
+import sp2 from "../semester-project2.jpg";
+import exam1 from "../exam1.JPG";
+import hackathon from "../hackathonJPG.JPG";
 
 function Featured() {
   return (
     <div className="highlights">
       <h3>Project highlights</h3>
       <div className="highlights__cont">
-        <div className="highlights__cont--item"></div>
-        <div className="highlights__cont--item"></div>
-        <div className="highlights__cont--item"></div>
+        <div className="highlights__cont--item">
+          <img src={sp2} alt="" />
+        </div>
+        <div className="highlights__cont--item">
+          <img src={exam1} alt="" />
+        </div>
+        <div className="highlights__cont--item">
+          <img src={hackathon} alt="" />
+        </div>
       </div>
     </div>
   );
