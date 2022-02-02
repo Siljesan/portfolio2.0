0a00f757b9f3cc8e7f7c04429d09a40e1b284bcb
Siljesandersen <silje_sandersen@hotmail.com>
2022-02-02 13:50:21 +0100

    fix: styling on project highlights

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -9,28 +9,40 @@ function Featured() {
       <h3>Project highlights</h3>
       <div className="highlights__cont">
         <div className="highlights__cont--item highlights-content">
-          <div class="highlights-content-overlay"></div>
-          <img src={sp2} alt="screenshot from project" />
-          <div class="highlights-details fadeIn-bottom">
-            <h3 class="highlights-title">Semester project</h3>
-            <p class="highlights-text">December 2021</p>
-          </div>
+          <a href="https://angry-bassi-1ce917.netlify.app/" target={"_blank"}>
+            <div class="highlights-content-overlay"></div>
+            <img src={sp2} alt="screenshot from project" />
+            <div class="highlights-details fadeIn-bottom">
+              <h3 class="highlights-title">Semester project</h3>
+              <p class="highlights-text">December 2021</p>
+            </div>
+          </a>
         </div>
         <div className="highlights__cont--item highlights-content">
-          <div class="highlights-content-overlay"></div>
-          <img src={exam1} alt="screenshot from project" />
-          <div class="highlights-details fadeIn-bottom">
-            <h3 class="highlights-title">Exam</h3>
-            <p class="highlights-text">June 2021</p>
-          </div>
+          <a
+            href="https://pedantic-curran-7bf149.netlify.app/"
+            target={"_blank"}
+          >
+            <div class="highlights-content-overlay"></div>
+            <img src={exam1} alt="screenshot from project" />
+            <div class="highlights-details fadeIn-bottom">
+              <h3 class="highlights-title">Exam</h3>
+              <p class="highlights-text">June 2021</p>
+            </div>
+          </a>
         </div>
         <div className="highlights__cont--item highlights-content">
-          <div class="highlights-content-overlay"></div>
-          <img src={hackathon} alt="screenshot from project" />
-          <div class="highlights-details fadeIn-bottom">
-            <h3 class="highlights-title">Hackathon</h3>
-            <p class="highlights-text">spring semester 2021</p>
-          </div>
+          <a
+            href="https://friendly-shockley-b1d933.netlify.app/"
+            target={"_blank"}
+          >
+            <div class="highlights-content-overlay"></div>
+            <img src={hackathon} alt="screenshot from project" />
+            <div class="highlights-details fadeIn-bottom">
+              <h3 class="highlights-title">Hackathon</h3>
+              <p class="highlights-text">spring semester 2021</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
