04ef210e228cef34b4467c7d77f07ca41baf442b
Siljesandersen <silje_sandersen@hotmail.com>
2022-05-30 14:59:55 +0200

    fix: api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:1337/";
+export const BASE_URL = "https://portfolio-api-siljes.herokuapp.com/";
 export const CONTACT_URL = `${BASE_URL}api/contacts/`;
 export const EMAIL_URL = `${BASE_URL}api/email/`;
